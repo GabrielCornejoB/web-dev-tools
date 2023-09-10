3ab16b583ef10fe204f372ee3fb84d64794b9e13
GabrielCornejoB <gabrielcorbot@gmail.com>
2023-09-10 13:19:04 -0500

    Navbar done & Side Drawer skeleton

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        dark: {
+          ...require("daisyui/src/theming/themes")["[data-theme=dark]"],
+          "base-100": "#18181b",
+          "base-content": "#b8b9c1",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
+};
