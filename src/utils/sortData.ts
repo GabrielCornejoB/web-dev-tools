17bde50eb6dee4bde414672a856777799cf0e1af
GabrielCornejoB <gabrielcorbot@gmail.com>
2023-09-18 17:53:27 -0500

    Sort algorythm for Tools in Main Page

---
+++ src/utils/sortData.ts
@@ -0,0 +1,31 @@
+import type { Tool } from "../models/tool.model";
+
+export function sortTools(data: Tool[]): Tool[] {
+  const categories: Record<string, Tool[]> = {
+    icons: [],
+    fonts: [],
+    illustrations: [],
+    colors: [],
+    libraries: [],
+    generators: [],
+    other: [],
+  };
+
+  data.forEach((obj) => {
+    categories[obj.category].push(obj);
+  });
+
+  const orderedTools: Tool[] = [];
+  for (const category in categories) {
+    orderedTools.push(...categories[category].slice(0, 4));
+  }
+
+  orderedTools.sort(() => Math.random() - 0.5);
+
+  const disorderedTools: Tool[] = data.filter(
+    (tool) => !orderedTools.includes(tool),
+  );
+  disorderedTools.sort(() => Math.random() - 0.5);
+
+  return [...orderedTools, ...disorderedTools];
+}
