2af19dcab3f98fe9f9fdbc7d1c6c2755528b4a35
GabrielCornejoB <gabrielcorbot@gmail.com>
2023-09-30 19:00:05 -0500

    Home Description

--- src/data/nav-links.ts
+++ src/data/nav-links.ts
@@ -2,45 +2,59 @@ import type { Link } from "../models/link.model";
 
 export const links: Link[] = [
   {
+    name: "Web Dev Tools",
+    path: "/",
+    fillColor: "",
+    hoverFillColor: "",
+  },
+  {
     name: "Icons",
     path: "/icons",
     fillColor: "fill-rose-500",
     hoverFillColor: "hover:fill-rose-500",
+    description:
+      "Welcome to the 'Icons' page - your gateway to a vast icon library. Explore curated links to top icon resources for all your web development needs. Say goodbye to icon hunting; we've got you covered!",
   },
   {
     name: "Fonts",
     path: "/fonts",
     fillColor: "fill-yellow-500",
     hoverFillColor: "hover:fill-yellow-500",
+    description: "uwu",
   },
   {
     name: "Illustrations",
     path: "/illustrations",
     fillColor: "fill-green-500",
     hoverFillColor: "hover:fill-green-500",
+    description: "uwu",
   },
   {
     name: "Colors",
     path: "/colors",
     fillColor: "fill-indigo-500",
     hoverFillColor: "hover:fill-indigo-500",
+    description: "uwu",
   },
   {
     name: "Libraries",
     path: "/libraries",
     fillColor: "fill-sky-500",
     hoverFillColor: "hover:fill-sky-500",
+    description: "uwu",
   },
   {
     name: "Generators",
     path: "/generators",
     fillColor: "fill-orange-500",
     hoverFillColor: "hover:fill-orange-500",
+    description: "uwu",
   },
   {
     name: "Other Tools",
     path: "/other",
     fillColor: "fill-fuchsia-500",
     hoverFillColor: "hover:fill-fuchsia-500",
+    description: "uwu",
   },
 ];
